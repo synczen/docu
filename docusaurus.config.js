4450d5061044cc84efbcf08c87310900d46b4c85
synczen <46060323+synczen@users.noreply.github.com>
2024-12-21 23:06:15 +0900

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Lee', // Usually your GitHub org/user name.
-  projectName: 'synczen', // Usually your repo name.
+  projectName: 'docu', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
